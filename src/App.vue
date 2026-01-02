9a559686cdcba0d596da7f82e23d97e07cb1655a
Matt Miller <matt@mattmiller.dev>
2026-01-02 10:19:32 -0600

    feat: basic site

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
+
+body {
+  background-color: #f9f9f9;
+  color: #151b1f;
+  font-family: 'Comfortaa', serif;
+  font-optical-sizing: auto;
+  font-weight: 300;
+  font-style: normal;
+  background-image: linear-gradient(to bottom, #f9f9f9, #f2f2f2, #ebebeb, #e5e5e5, #dedede);
+  overflow: hidden;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+}
+
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
+}
+
+.glass {
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border-radius: 1rem;
+  background: rgb(255, 255, 255, 0.3);
+  padding: 1.5rem;
+}
+
+/* Grow Shadow */
+.hvr-grow-shadow {
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-property: box-shadow, transform;
+  transition-property: box-shadow, transform;
+}
+.hvr-grow-shadow:hover,
+.hvr-grow-shadow:focus,
+.hvr-grow-shadow:active {
+  box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.5);
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>It is currently March {{ numberWithCommas(daysSince) }}{{ nth(daysSince) }}, 2022</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const daysSince = computed(() => {
+  const startDate = new Date('2020-03-13')
+  const currentDate = new Date()
+  const diffTime = Math.abs(currentDate.getTime() - startDate.getTime())
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+  return diffDays + 13
+})
+
+function nth(d) {
+  const dString = String(d)
+  const last = +dString.slice(-2)
+  if (last > 3 && last < 21) return 'th'
+  switch (last % 10) {
+    case 1:
+      return 'st'
+    case 2:
+      return 'nd'
+    case 3:
+      return 'rd'
+    default:
+      return 'th'
+  }
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+}
+</script>
